be0fa170eacd9294495f0a0fe47bba07448dfd04
Artem Baskal <a.baskal@adguard.com>
2019-10-22 18:08:04 +0300

    add nativeMessaing permission in chrome manifest

--- src/background/index.js
+++ src/background/index.js
@@ -1,24 +1,24 @@
-const port = window.chrome.runtime.connectNative('native_browser_assistant');
+const port = chrome.runtime.connectNative('native_browser_assistant');
 
 port.onMessage.addListener(({
     id, data, parameters, appState, result,
 }) => {
-    console.log(`Received: id = ${id}, data = ${data || null}, parameters = ${JSON.stringify(parameters)}, appState = ${JSON.stringify(appState)}, result = ${result}`);
+    console.log(`Received: id = ${id}, data = ${data || 'no data received'}, parameters = ${JSON.stringify(parameters)}, appState = ${JSON.stringify(appState)}, result = ${result}`);
 });
 
-window.chrome.browserAction.onClicked.addListener(() => {
-    const id = Math.floor(Math.random() * 1000);
-    console.log(`Sending:  id = ${id}`);
-    const requestMessage = {
-        id,
-        type: 'init',
-        data: '2019-10-20T08:00:00.000Z',
-        parameters: {
-            version: '1.2.3.5',
-            apiVersion: '3',
-            userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36',
-            type: 'native_browser_assistant',
-        },
-    };
-    port.postMessage(requestMessage);
-});
+// chrome.browserAction.onClicked.addListener(() => {
+const id = Math.floor(Math.random() * 1000);
+console.log(`Sending:  id = ${id}`);
+const requestMessage = {
+    id,
+    type: 'init',
+    data: '2019-10-20T08:00:00.000Z',
+    parameters: {
+        version: '1.2.3.5',
+        apiVersion: '3',
+        userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36',
+        type: 'nativeAssistant',
+    },
+};
+port.postMessage(requestMessage);
+// });
