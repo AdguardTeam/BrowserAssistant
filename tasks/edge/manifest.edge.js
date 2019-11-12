34c52bb22a81f975deb21bd30476c8959e4b580f
Artem Baskal <a.baskal@adguard.com>
2019-11-12 16:41:12 +0300

    delete permissons from manifests

--- tasks/edge/manifest.edge.js
+++ tasks/edge/manifest.edge.js
@@ -2,15 +2,7 @@ module.exports = {
     minimum_edge_version: '40.15063.0.0',
     options_page: 'options.html',
     permissions: [
-        'tabs',
-        '<all_urls>',
-        'webRequest',
-        'webRequestBlocking',
-        'webNavigation',
-        'storage',
-        'contextMenus',
-        'cookies',
-        'privacy',
+        'activeTab',
         'nativeMessaging',
     ],
 };
