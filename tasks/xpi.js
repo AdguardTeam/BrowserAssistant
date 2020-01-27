7387486eea610bfbe5e0d82d8d897f9845e2d9b7
ArtemBaskal <a.baskal@adguard.com>
2020-01-27 16:10:11 +0300

    change code style

--- tasks/xpi.js
+++ tasks/xpi.js
@@ -1,23 +1,26 @@
 /* eslint-disable no-console */
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const webExt = require('web-ext');
 const credentials = require('../private/AdguardBrowserAssistant/mozilla_credentials.json');
 const { BROWSER_TYPES, ENV_MAP } = require('./consts');
 
-const { AMO_JWT_ISSUER, AMO_JWT_SECRET } = credentials;
+const { apiKey, apiSecret } = credentials;
 const { NODE_ENV } = process.env;
 
 async function generateXpi() {
     try {
         const sourceDir = `build/${ENV_MAP[NODE_ENV].outputPath}/${BROWSER_TYPES.FIREFOX}`;
         const artifactsDir = `build/${ENV_MAP[NODE_ENV].outputPath}`;
-        const command = `yarn web-ext sign --api-key ${AMO_JWT_ISSUER} --api-secret ${AMO_JWT_SECRET} -s ${sourceDir} -a ${artifactsDir}`;
 
-        console.log(command);
-        const { stdout, stderr } = await exec(command);
+        const res = await webExt.default.cmd.sign({
+            apiKey,
+            apiSecret,
+            sourceDir,
+            artifactsDir,
+        }, {
+            shouldExitProgram: false,
+        });
 
-        console.log('stdout:', stdout);
-        console.log('stderr:', stderr);
+        console.log(res);
     } catch (error) {
         console.error(error.message);
     }
