8a5cfce10ab2e6522f0969382cb5a4f16cfe3c90
ArtemBaskal <a.baskal@adguard.com>
2020-01-30 20:55:34 +0300

    repair translation script, fix linter warnings

--- tasks/locales.js
+++ tasks/locales.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-console, no-await-in-loop */
-const { promises: fs } = require('fs');
+const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
 const FormData = require('form-data');
@@ -56,7 +56,7 @@ function saveFile(filePath, data) {
     if (!fs.existsSync(dirPath)) {
         fs.mkdirSync(dirPath);
     }
-    return fs.writeFile(filePath, data);
+    return fs.promises.writeFile(filePath, data);
 }
 
 /**
