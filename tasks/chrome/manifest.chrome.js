3ce78d5ef39a07c4a2362a353ab4bfef7a806e19
Andrey Meshkov <am@adguard.com>
2020-02-04 13:38:40 +0300

    fix: 🐛 removed options page, bump version to 0.0.12

--- tasks/chrome/manifest.chrome.js
+++ tasks/chrome/manifest.chrome.js
@@ -1,6 +1,5 @@
 module.exports = {
     minimum_chrome_version: '55.0',
-    options_page: 'options.html',
     permissions: [
         'activeTab',
         'nativeMessaging',
--- tasks/edge/manifest.edge.js
+++ tasks/edge/manifest.edge.js
@@ -1,6 +1,5 @@
 module.exports = {
     minimum_edge_version: '40.15063.0.0',
-    options_page: 'options.html',
     permissions: [
         'activeTab',
         'nativeMessaging',
