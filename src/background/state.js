ab4ed1ee391e25e0f4e47551f00a7483e2656a74
tvinzz <o.labudko@adguard.com>
2021-08-24 04:09:39 +0300

    handled newtab and extensions page for getCurrentFilteringState

--- src/background/state.js
+++ src/background/state.js
@@ -1,3 +1,5 @@
+/* global chrome */
+
 import isEqual from 'lodash/isEqual';
 import throttle from 'lodash/throttle';
 import browser from 'webextension-polyfill';
@@ -11,6 +13,8 @@ import {
 } from '../lib/helpers';
 import { PROTOCOLS } from '../popup/stores/consts';
 
+const NEW_TAB_URL = chrome ? 'chrome://newtab/' : 'about:newtab';
+const EXTENSIONS_TAB_URL = chrome ? 'chrome://extensions/' : 'about:addons';
 /**
  * This class handles app state
  * All requests to the native host should be made through this class
@@ -264,7 +268,10 @@ class State {
         this.updateSecured(url);
 
         // Do not send empty urls or non http urls, see - AG-2360
-        if (!url || !isHttp(url)) {
+        if (!(url && isHttp(url))
+            // AG-9738
+            && url !== NEW_TAB_URL
+            && url !== EXTENSIONS_TAB_URL) {
             return null;
         }
 
