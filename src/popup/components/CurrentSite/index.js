fcbd475baa4f94e2cefa9b3c0df50060ed3a3e87
tvinzz <o.labudko@adguard.com>
2021-10-18 16:47:40 +0300

    fixed triangle display for modal

--- src/popup/components/CurrentSite/index.js
+++ src/popup/components/CurrentSite/index.js
@@ -130,8 +130,9 @@ const CurrentSite = observer(() => {
                     onMouseOut={getHandlerForHttpSite(handleCertStatusModalState)}
                 >
                     {!pageProtocol.isSecured
-                    && (shouldOpenCertStatusModal
-                        || (!pageProtocol.isHttps && isPageStatusModalOpen))
+                        && (shouldOpenCertStatusModal
+                            || (!pageProtocol.isHttps
+                                && (isPageStatusModalOpen || isCertStatusModalOpen)))
                     && <div className="arrow-up" />}
                 </div>
                 <h2 tabIndex={uiStore.globalTabIndex} className="current-site__name">
