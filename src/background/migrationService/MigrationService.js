7beff904832281d3ce3c5b2b3de7ee88491fc798
tvinzz <o.labudko@adguard.com>
2022-01-27 20:56:25 +0700

    fixed storage migration condition in MigrationService

--- src/background/migrationService/MigrationService.js
+++ src/background/migrationService/MigrationService.js
@@ -3,6 +3,7 @@ import { compare } from 'compare-versions';
 import { localStorage } from '../localStorage';
 import log from '../../lib/logger';
 import { consent } from '../consent';
+import browserApi from '../../lib/browserApi';
 
 const STORAGE_MIGRATION_VERSION = '1.2.2';
 
@@ -10,7 +11,7 @@ export class MigrationService {
     async migrate(previousVersion) {
         // consent setting moved from local storage to
         // browser storage after version 1.2.2 in firefox only
-        if (consent.CONSENT_REQUIRED_STORAGE_KEY
+        if (browserApi.utils.isFirefoxBrowser
             && compare(previousVersion, STORAGE_MIGRATION_VERSION, '<=')) {
             await this.storageMigrationForFirefox();
         }
