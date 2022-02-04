0c463e2bb260022da7a108d1e8d5dc19f6dc70e1
tvinzz <o.labudko@adguard.com>
2022-02-04 19:02:36 +0700

    fixed migration from v1.2.1 or earlier

--- src/background/migrationService/MigrationService.js
+++ src/background/migrationService/MigrationService.js
@@ -12,21 +12,43 @@ export class MigrationService {
         // consent setting moved from local storage to
         // browser storage after version 1.2.2 in firefox only
         if (browserApi.utils.isFirefoxBrowser
-            && compare(previousVersion, STORAGE_MIGRATION_VERSION, '<=')) {
-            await this.storageMigrationForFirefox();
+            && compare(previousVersion, STORAGE_MIGRATION_VERSION, '=')) {
+            await this.consentSettingMigration();
+        }
+
+        // consent agreement migration if updated from v1.2.1 or earlier
+        if (browserApi.utils.isFirefoxBrowser
+            && compare(previousVersion, STORAGE_MIGRATION_VERSION, '<')) {
+            await this.consentAgreementMigration();
         }
     }
 
     /**
-     * Migration from local storage to browser storage
+     * Migration consent setting from local storage to browser storage
      */
-    storageMigrationForFirefox = async () => {
+    consentSettingMigration = async () => {
         const isConsentRequired = JSON.parse(
             localStorage.get(consent.CONSENT_REQUIRED_STORAGE_KEY)
         );
+        log.debug(`Consent setting in local storage: ${isConsentRequired}`);
+
         if (isConsentRequired !== undefined) {
             log.debug('Migrate consent setting from local storage to browser storage');
             await consent.setConsentRequired(isConsentRequired);
         }
+    };
+
+    /**
+     * Migration from local storage to browser storage
+     */
+    consentAgreementMigration = async () => {
+        // consent agreement storage key (deprecated)
+        const AGREEMENT_STORAGE_KEY = 'consent.agreement';
+
+        const isConsentRequired = JSON.parse(localStorage.get(AGREEMENT_STORAGE_KEY));
+        log.debug(`Consent agreement setting in local storage: ${isConsentRequired}`);
+
+        await consent.setConsentRequired(!isConsentRequired);
+        log.debug(`Set consent setting to browser storage: ${!isConsentRequired}`);
     };
 }
