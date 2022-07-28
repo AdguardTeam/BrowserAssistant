2de528bc83d2874fc563a0e839a93b93ab113906
tvinzz <o.labudko@adguard.com>
2022-07-28 14:53:44 +0300

    replaced links

--- FIREFOX_BETA.md
+++ FIREFOX_BETA.md
@@ -2,5 +2,5 @@
 
 The beta version of AdGuard Assistant Beta for Firefox is distributed as a standalone XPI file that you need to install manually.
 
-[Click to install assistant](https://static.adguard.com/extensions/browserassistant/beta/firefox.xpi)
+[Click to install assistant](https://static.adtidy.org/extensions/browserassistant/beta/firefox.xpi)
 
--- src/background/api/stubHostApi.js
+++ src/background/api/stubHostApi.js
@@ -14,7 +14,7 @@ let hostData = {
     version: '7.3.2496',
     apiVersion: '3',
     isValidatedOnHost: true,
-    reportUrl: 'https://reports.adguard.com/en/new_issue.html?url=http://example.org/',
+    reportUrl: 'https://link.adtidy.org/forward.html?action=report_debug&from=popup&app=browser_assistant',
     appState: {
         isRunning: true,
         isProtectionEnabled: true,
